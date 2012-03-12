ae0c901365e4166e80c5624abc3f8fa15804e8e3
Diogo Uchoas <diogouchoas@gmail.com>
2012-03-12 00:39:22 -0300

    organização do codigo

--- logout.php
+++ logout.php
@@ -1,8 +1,7 @@
 <?php
-    ob_start();
-	
-    session_start();
-	session_destroy();
-	header('Location: loginSel.php');
+ob_start();
+session_start();
+session_destroy();
+header('Location: loginSel.php');
 ob_flush();
 ?>
