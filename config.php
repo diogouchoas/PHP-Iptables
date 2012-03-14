16d4a9c0273322a8485611b0ed0ae11e8a2a5d80
Diogo <diogouchoas@gmail.com>
2012-03-13 23:34:18 -0300

    retirei as conf da anima

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
 $config_array = array(
-	'account_suffix' => "@anima.intranet",
-	'base_dn' =>	"DC=anima,DC=intranet",
-	'domain_controllers' => array("172.17.2.10"),
+	'account_suffix' => "@local.intranet",
+	'base_dn' =>	"DC=local,DC=intranet",
+	'domain_controllers' => array(192.168.1.1""),
 	'access_group' => "firewall.system",
 	'session_expire' => 5
 );
